36dc49b9884604559e8d8da43d4cf26513fd82d5
Shovy <jcusiq@unsa.edu.pe>
2026-05-19 16:49:47 -0500

    Lab 11

--- Ej01.py
+++ Ej01.py
@@ -1,5 +1,22 @@
-num = int(input("Ingrese un num (entero): "))
-if num % 2 == 0:
-    print("Es par")
+##desigualdad triangular
+lado1 = float(input("Ingrese el 1er lado del triángulo "))
+lado2 = float(input("Ingrese el 2do lado del triángulo "))
+lado3 = float(input("Ingrese el 3er lado del triángulo "))
+esTriangulo = True
+if lado1 + lado2 < lado3:
+    esTriangulo = False
+if lado2 + lado3 < lado1:
+    esTriangulo = False
+if lado1 + lado3 < lado2:
+    esTriangulo = False
+tipo = ""
+if esTriangulo:
+    if lado1 == lado2 == lado3:
+        tipo = "equilatero"
+    elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
+        tipo = "isoceles"
+    else:
+        tipo = "escaleno"
+    print(f"El triangulo es {tipo}")
 else:
-    print("No es par")
+    print("Eso no es un triangulo")
--- Ej02.py
+++ Ej02.py
@@ -1,7 +1,25 @@
-peso = float(input("Ingrese su peso (en Kg): "))
-altura = float(input("Ingrese su altura (en mts): "))
-IMC = peso / (altura * altura)
-if IMC < 25:
-    print("Tas bien bru")
+#coordenadas, froman un triangulo?, es escaleno isoceles o equilatero
+import math
+x1 = float(input("Ingrese la 1° coordenada del 1° punto "))
+y1 = float(input("Ingrese la 2° coordenada del 1° punto "))
+x2 = float(input("Ingrese la 1° coordenada del 2° punto "))
+y2 = float(input("Ingrese la 2° coordenada del 2° punto "))
+x3 = float(input("Ingrese la 1° coordenada del 3° punto "))
+y3 = float(input("Ingrese la 2° coordenada del 3° punto "))
+
+esTriangulo = True
+if x1 == x2 == x3 or y1 == y2 == y3:
+    esTriangulo = False
+if esTriangulo:
+    lado1 = math.dist((x1, y1), (x2, y2))
+    lado2 = math.dist((x1, y1), (x3, y3))
+    lado3 = math.dist((x2, y2), (x3, y3))
+    if lado1 == lado2 == lado3:
+        tipo = "equilatero"
+    elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
+        tipo = "isoceles"
+    else:
+        tipo = "escaleno"
+    print(f"El triangulo es {tipo}")
 else:
-    print("Bro tienes sobrepeso :v")
+    print("Eso no es un triangulo")
